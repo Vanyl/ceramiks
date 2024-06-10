f96c3794d21f2dbbcbdaae61b113e4ad4fc39829
Yavanna Perez <yavannaperez@hotmail.com>
2024-06-10 16:57:26 +0200

    test side nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 //import './sass/style.scss'
+import SideMenu from "../components/SideMenu";
 
 function Home() {
+
+    const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+    const openSideMenu = () => {
+        console.log(isSideMenuOpen)
+        setIsSideMenuOpen(!isSideMenuOpen)
+    }
     return (
         <div>
-            <p>Helloooooo !</p>
-            <button>Click me</button>
+            <button onClick={openSideMenu}>Open Side Menu</button>
+            <SideMenu isOpen={isSideMenuOpen} setIsOpen={setIsSideMenuOpen} />
         </div>
     )
 }
