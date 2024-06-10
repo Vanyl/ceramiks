d267c8349551cb7754f31525c243e49fd0be3b06
Myriiam <kadiimyriam@hotmail.com>
2024-06-10 17:08:34 +0200

    Add general menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 //import './sass/style.scss'
+import Navbar from '../components/Navbar.jsx'
+import '../sass/home.scss'
+
 
 function Home() {
     return (
-        <div>
-            <p>Helloooooo !</p>
-            <button>Click me</button>
+        <div className='home-container'>
+            <Navbar />
+            <div>
+                <p>Helloooooo !</p>
+                <button>Click me</button>
+            </div>
         </div>
     )
 }
