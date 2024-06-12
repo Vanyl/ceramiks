bfc2d0aae7150799b25d1a7f6d6b84e6317cfaa7
Myriiam <kadiimyriam@hotmail.com>
2024-06-12 18:59:40 +0200

    Fix search bar on auth pages

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -14,24 +14,26 @@ const Register = () => {
 
     return (
         <>  
-            <div className='auth-container'>   
-                <h2>REGISTER</h2>
-                <form className='auth-form' onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register("firstname", { required: "The first name is required" })} type='text' placeholder='First name'/>
-                    <p>{errors.firstname && errors.firstname.message}</p>
-                    <input {...register("lastname", { required: "The last name is required" })} type='text' placeholder='Last name'/>
-                    <p>{errors.lastname && errors.lastname.message}</p>
-                    <input {...register("email", { required: "The email is required" })} type='email' placeholder='Email'/>
-                    <p>{errors.email && errors.email.message}</p>
-                    <input {...register("password", { required: "The password is required" })} type='password' placeholder='Password'/>
-                    <p>{errors.password && errors.password.message}</p>
-                    <button className='auth-btn' disabled={isSubmitting} type='submit'>
-                        {isSubmitting ? "Loading..." : "CREATE MY ACCOUNT"}
-                    </button>
-                </form>
-                <div className='go-login'>
-                    <p>Already Have An Account ?</p>
-                    <Link to="/login" className=''>Login</Link>
+            <div className='page-container'>
+                <div className='auth-container'>   
+                    <h2>REGISTER</h2>
+                    <form className='auth-form' onSubmit={handleSubmit(onSubmit)}>
+                        <input {...register("firstname", { required: "The first name is required" })} type='text' placeholder='First name'/>
+                        <p>{errors.firstname && errors.firstname.message}</p>
+                        <input {...register("lastname", { required: "The last name is required" })} type='text' placeholder='Last name'/>
+                        <p>{errors.lastname && errors.lastname.message}</p>
+                        <input {...register("email", { required: "The email is required" })} type='email' placeholder='Email'/>
+                        <p>{errors.email && errors.email.message}</p>
+                        <input {...register("password", { required: "The password is required" })} type='password' placeholder='Password'/>
+                        <p>{errors.password && errors.password.message}</p>
+                        <button className='auth-btn' disabled={isSubmitting} type='submit'>
+                            {isSubmitting ? "Loading..." : "CREATE MY ACCOUNT"}
+                        </button>
+                    </form>
+                    <div className='go-login'>
+                        <p>Already Have An Account ?</p>
+                        <Link to="/login" className='login-link'>Login</Link>
+                    </div>
                 </div>
             </div>
         </>
