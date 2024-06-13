b607b0d104aa28b8a43c8e82c5fbb95fb65dbae2
Yavanna Perez <yavannaperez@hotmail.com>
2024-06-13 09:57:38 +0200

    add footer to router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.jsx";
+import Footer from "../components/Footer.jsx"
 
 function AppRouter() {
     return (
@@ -7,6 +8,7 @@ function AppRouter() {
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
+            <Footer/>
         </HashRouter>
     )
 }
