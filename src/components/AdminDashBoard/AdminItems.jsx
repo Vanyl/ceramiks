2a4febd8128dbf28f814eec47d579f0e26ca9e38
Yavanna Perez <yavannaperez@hotmail.com>
2024-07-17 14:27:10 +0200

    admin dashboard basic version

---
+++ src/components/AdminDashBoard/AdminItems.jsx
@@ -0,0 +1,8 @@
+const AdminItems = () => {
+    
+    return (
+        <p style={{ marginTop: '250px' }}>items</p>
+    )
+}
+
+export default AdminItems
---
+++ src/components/AdminDashBoard/AdminUsers.jsx
@@ -0,0 +1,8 @@
+const AdminUsers = () => {
+    
+    return (
+        <p style={{ marginTop: '250px' }}>users</p>
+    )
+}
+
+export default AdminUsers
