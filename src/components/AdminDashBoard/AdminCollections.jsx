e00d45db89206cd42fde3b9acb15b99d0d5d7b9f
Yavanna Perez <yavannaperez@hotmail.com>
2024-07-18 11:54:26 +0200

    edit collection feature working

--- src/components/AdminDashBoard/AdminCollections.jsx
+++ src/components/AdminDashBoard/AdminCollections.jsx
@@ -4,21 +4,25 @@ import { useAuth } from '../../context/authContext.jsx';
 
 
 const AdminCollections = () => {
+    const { authState } = useAuth();
     const { allCollections, isLoading } = useContext(CollectionsContext);
     const [showAddForm, setShowAddForm] = useState(false)
+    const [showEditForm, setShowEditForm] = useState(false)
     const [newCollection, setNewCollection] = useState('')
-    const { authState } = useAuth();
-    console.log(authState)
-    console.log(typeof authState.token)
-    
+    const [collectionToEdit, setCollectionToEdit] = useState(null);
+    const [nameCollectionEdited, setNameCollectionEdited] = useState('')
+
     const handleAddClick = () => {
         setShowAddForm(true);
     };
-    //https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/add/collection (post) name obligé
-    //https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/patch/collection/:id (patch) name obligé
-    //https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/delete/collections/:id (delete)
 
-    const handleFormSubmit = async (e) => {
+    const handleEditClick = (collection) => {
+        setCollectionToEdit(collection.id); //collectionToEdit will be an ID or null
+        setNameCollectionEdited(collection.name)
+        setShowEditForm(true);
+    };
+
+    const addNewCollection = async (e) => {
         e.preventDefault();
         try {
             const response = await fetch('https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/add/collection', {
@@ -42,26 +46,96 @@ const AdminCollections = () => {
         }
     };
 
+    const deleteCollection = async (id) => {
+        // e.preventDefault();
+        console.log(id)
+        try {
+            const response = await fetch(`https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/delete/collections/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${authState.token}`
+                },
+            });
+
+            if (response.ok) {
+                //
+            } else {
+                const errorData = await response.json();
+                console.error('Error deleting collection:', errorData);
+            }
+        } catch (error) {
+            console.error('Error deleting collection:', error);
+        }
+    };
+
+    const onDelete = (id) => {
+        console.log(id)
+        deleteCollection(id);
+    };
+
+    const editCollection = async (e) => {
+        e.preventDefault();
+        if (collectionToEdit !== null) {
+            try {
+                const response = await fetch(`https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/patch/collection/${collectionToEdit}`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${authState.token}`
+                    },
+                    body: JSON.stringify({ name: nameCollectionEdited }),
+                });
+    
+                if (response.ok) {
+                    setShowEditForm(false);
+                    setCollectionToEdit(null);
+                    setNameCollectionEdited('')
+                } else {
+                    console.error('Error editing collection:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error editing collection:', error);
+            }
+        }
+    }
+
 
     return (
         <div style={{ marginTop: '250px' }}>
             {isLoading ? (
                 <p>Loading...</p>
             ) : (
-                <div style={{display: 'flex', gap:'25px', justifyContent: 'center'}}>
+                <>
+                <div style={{ display: 'flex', gap: '25px', justifyContent: 'center' }}>
                     {allCollections?.map((collection) => (
-                        <div key={collection.id} className="collection-card" style={{display: 'flex', gap:'25px', alignContent: 'center', flexDirection: 'column', border: '1px solid black'}}>
+                        <div key={collection.id} className="collection-card" style={{ display: 'flex', gap: '25px', alignContent: 'center', flexDirection: 'column', border: '1px solid black' }}>
                             <p>{collection.name}</p>
                             <p>Created on {new Date(collection.date_created).toISOString().split('T')[0]}</p>
-                            <button>edit</button>
-                            <button>delete</button>
+                            <button onClick={() => handleEditClick(collection)}>edit</button>
+                            <button onClick={() => onDelete(collection.id)}>delete</button>
                         </div>
                     ))}
                 </div>
+                {showEditForm && (
+                        <form onSubmit={editCollection} style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <input
+                                type="text"
+                                name={nameCollectionEdited}
+                                value={nameCollectionEdited}
+                                onChange={(e) => setNameCollectionEdited(e.target.value)}
+                                placeholder="Collection Name"
+                                required
+                                style={{ marginBottom: '10px', padding: '5px', fontSize: '16px' }}
+                            />
+                            <button type="submit" style={{ padding: '5px 10px', fontSize: '16px' }}>Edit</button>
+                        </form>
+                    )}
+                </>
             )}
-            <button onClick={handleAddClick}>add collection</button>
+            <button onClick={handleAddClick} style={{ marginTop: '20px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }}>add collection</button>
             {showAddForm && (
-                <form onSubmit={handleFormSubmit} style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <form onSubmit={addNewCollection} style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                     <input
                         type="text"
                         name={newCollection}
