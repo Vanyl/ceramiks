7d7f2bbfec23c464cf600ecdca3a074e4306a815
Myriiam <kadiimyriam@hotmail.com>
2024-07-24 21:10:30 +0200

    Fix upload bug for other images

--- src/components/AdminDashBoard/AdminItems.jsx
+++ src/components/AdminDashBoard/AdminItems.jsx
@@ -249,7 +249,7 @@ const AdminItems = () => {
                                 validate: {isMultipleFiles: files => files.length === 2 || 'You must upload 2 images'}                                })} 
                                 type="file" 
                                 accept=".png, .jpg, .jpeg"
-                                name="images[]"
+                               // name="images[]"
                                 multiple
                             />
                             <p>{errors.images && errors.images.message}</p>
