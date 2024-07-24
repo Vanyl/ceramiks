bf60a422248ee310099549417afe74af729bd8d3
Myriiam <kadiimyriam@hotmail.com>
2024-07-24 20:04:28 +0200

    Fix upload other pictures

--- src/components/AdminDashBoard/AdminItems.jsx
+++ src/components/AdminDashBoard/AdminItems.jsx
@@ -169,15 +169,15 @@ const AdminItems = () => {
     
                 // Prepare FormData for additional images
                 const additionalImagesForm = new FormData();
-                /* Array.from(data.additionalImages).forEach((file, index) => {
+                Array.from(data.additionalImages).forEach((file, index) => {
                     additionalImagesForm.append(`images[${index}]`, file);
-                }); */
-                for (let i = 0; i < data.images.length; i++) {
+                });
+                /* for (let i = 0; i < data.images.length; i++) {
                     additionalImagesForm.append(`images[${i}]`, data.images[i]);
-                }
+                } */
     
                 // Send additional images to the route with the new item's ID
-                const additionalImagesResponse = await fetch(`https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/add/itemsimg/${newItem.id}`, {
+                const additionalImagesResponse = await fetch(`https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/add/itemsimg/${newItem.itemId}`, {
                     method: 'POST',
                     body: additionalImagesForm,
                     headers: {
