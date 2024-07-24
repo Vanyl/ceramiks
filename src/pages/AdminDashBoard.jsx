aff7e445353c5133ba2e3198cf41ae73724a68ac
Yavanna Perez <yavannaperez@hotmail.com>
2024-07-24 23:07:52 +0200

    redirect if not admin

--- src/pages/AdminDashBoard.jsx
+++ src/pages/AdminDashBoard.jsx
@@ -1,5 +1,7 @@
 import '../sass/admin-dashboard.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/authContext.jsx';
 import AdminItems from '../components/AdminDashBoard/AdminItems.jsx'
 import AdminCollections from '../components/AdminDashBoard/AdminCollections.jsx'
 import AdminUsers from '../components/AdminDashBoard/AdminUsers.jsx'
@@ -9,30 +11,45 @@ const AdminDashBoard = () => {
     //security seul admin a access to this route
     const [activeSection, setActiveSection] = useState('welcome');
     const [showMobileNav, setShowMobileNav] = useState(false);
+    const navigate = useNavigate();
+    const { authState, logout } = useAuth();
+
 
     const toggleMobileNav = () => {
         setShowMobileNav(!showMobileNav);
     };
 
+    useEffect(() => {
+        if (!authState.is_admin) {
+            navigate('/'); // Redirect to home or another page if not admin
+        }
+    }, [authState, navigate]);
+
     return (
         <div>
-            <nav className="dashboard-nav">
-                <button className="toggle-button" onClick={toggleMobileNav}>Admin Dashboard</button>
-                <ul className={`nav-links ${showMobileNav ? 'show' : ''}`}>
-                    <li><p onClick={() => setActiveSection('admin-items')}>All Items</p></li>
-                    <li><p onClick={() => setActiveSection('admin-collections')}>All Collections</p></li>
-                    <li><p onClick={() => setActiveSection('admin-users')}>All Users</p></li>
-                    <li><p onClick={() => setActiveSection('admin-orders')}>All Orders</p></li>
-                </ul>
-            </nav>
-            <div>
-                <h1>Welcome admin</h1>
-                {activeSection === 'welcome' && (<h1>Welcome Admin</h1>)}
-                {activeSection === 'admin-items' && (<AdminItems />)}
-                {activeSection === 'admin-collections' && (<AdminCollections />)}
-                {activeSection === 'admin-users' && (<AdminUsers />)}
-                {activeSection === 'admin-orders' && (<AdminOrders />)}
-            </div>
+            {authState.is_admin ? (
+                <>
+                    <nav className="dashboard-nav">
+                        <button className="toggle-button" onClick={toggleMobileNav}>Admin Dashboard</button>
+                        <ul className={`nav-links ${showMobileNav ? 'show' : ''}`}>
+                            <li><p onClick={() => setActiveSection('admin-items')}>All Items</p></li>
+                            <li><p onClick={() => setActiveSection('admin-collections')}>All Collections</p></li>
+                            <li><p onClick={() => setActiveSection('admin-users')}>All Users</p></li>
+                            <li><p onClick={() => setActiveSection('admin-orders')}>All Orders</p></li>
+                        </ul>
+                    </nav>
+                    <div>
+                        <h1>Welcome admin</h1>
+                        {activeSection === 'welcome' && (<h1>Welcome Admin</h1>)}
+                        {activeSection === 'admin-items' && (<AdminItems />)}
+                        {activeSection === 'admin-collections' && (<AdminCollections />)}
+                        {activeSection === 'admin-users' && (<AdminUsers />)}
+                        {activeSection === 'admin-orders' && (<AdminOrders />)}
+                    </div>
+                </>
+            ) : (
+                <p>Redirecting...</p>
+            )}
         </div>
     );
 }
