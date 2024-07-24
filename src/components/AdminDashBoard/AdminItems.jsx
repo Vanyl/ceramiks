b2579f414d3ab8cadf590c48a1eadd94e41b5c9f
Myriiam <kadiimyriam@hotmail.com>
2024-07-24 20:16:18 +0200

    Fix upload again

--- src/components/AdminDashBoard/AdminItems.jsx
+++ src/components/AdminDashBoard/AdminItems.jsx
@@ -169,9 +169,13 @@ const AdminItems = () => {
     
                 // Prepare FormData for additional images
                 const additionalImagesForm = new FormData();
-                for (let i = 0; i < data.images.length; i++) {
+                /* for (let i = 0; i < data.images.length; i++) {
                     additionalImagesForm.append('images', data.images[i]);
-                }
+                } */
+                   
+                    (data.additionalImages || []).forEach((file, index) => {
+                        additionalImagesForm.append(`images[${index}]`, data.images);
+                    });
     
                 // Send additional images to the route with the new item's ID
                 const additionalImagesResponse = await fetch(`https://ecommerce-website3333-593ff35538d5.herokuapp.com/admin/add/itemsimg/${newItem.itemId}`, {
